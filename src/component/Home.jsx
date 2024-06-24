42fcc3455c5a039b47c650bfde597d30cfdf867a
choaeri <choyd00@naver.com>
2024-06-24 20:08:26 +0900

    홈 수정

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,23 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
-import axios from "axios";
 import { AppContext } from "../context/AppContext";
 import { axiosInstance } from "../common/func/axios";
-import { notification } from "antd";
-import { FrownOutlined, SmileOutlined } from "@ant-design/icons";
+import './Home.css';
+import { Card } from "antd";
+import Meta from "antd/es/card/Meta";
 
 export default function Home() {
   const navigate = useNavigate();
-  const [userData, setUserData] = useState(null);
   const { isLogin, setIsLogin } = useContext(AppContext); // isLogin도 useContext로 부터 추출
 
   const goToLogin = () => {
     navigate("/login");
   };
 
-  // 인증 API 호출
   useEffect(() => {
     const axios = async () => {
+      // 인증 API 호출
       axiosInstance.get("api/auth/check")
         .then(response => {
           if (response.data.isAuthenticated) {
@@ -27,6 +26,14 @@ export default function Home() {
         .catch(err => {
           console.error("Error fetching user data:", err);
         });
+
+      // axiosInstance.post("api/posts")
+      //   .then((res) => {
+      //     console.log(res);
+      //   })
+      //   .catch(err => {
+      //     console.error("Error fetching user data:", err);
+      //   });
     };
     axios();
   }, []);
@@ -53,11 +60,42 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <button onClick={handleButtonClick}>{!isLogin ? '로그인' : '로그아웃'}</button>
-      <div>테스트 API 호출</div>
-      {userData && <div>인증 성공, username: {userData.username}</div>}
-      {!userData && <div>인증 전</div>}
+    <div className="DNAHome">
+      <div className="Header">
+        <button className="loginBtn" onClick={handleButtonClick}>{!isLogin ? '로그인' : '로그아웃'}</button>
+      </div>
+      <div className="RmdContent">
+        <button className="recommendBtn">추천받기</button>
+      </div>
+      <div className="CardContent">
+        <header>
+          <h2>Best Workation Recommend</h2>
+        </header>
+        <button className="onMap">map</button>
+        <div className="cardCnt">
+          <Card
+            className="card"
+            hoverable
+            cover={<img alt="sokcho" src={`https://cdn.woman.chosun.com/news/photo/202204/97384_80024_1440.jpg`} />}
+          >
+            <Meta title="Sokcho" />
+          </Card>
+          <Card
+            className="card"
+            hoverable
+            cover={<img alt="chuncheon" src={`https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/15/9dc7194a-30cc-4005-aabc-372e362b29f9.jpg`} />}
+          >
+            <Meta title="Chuncheon" />
+          </Card>
+          <Card
+            className="card"
+            hoverable
+            cover={<img alt="yangyang" src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZbXZWlokJE_MLQc1YqROH2doJihNzVIWxqg&s`} />}
+          >
+            <Meta title="Yangyang" />
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
