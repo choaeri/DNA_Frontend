02d8e74b7a7507585f8b90cff85e86a3cd267f92
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-08-12 00:04:14 +0900

    회원가입 입력 검증 완료

--- src/component/Header/login/Login.jsx
+++ src/component/Header/login/Login.jsx
@@ -106,10 +106,7 @@ export default function Login() {
             <Form.Item {...tailLayout}>
               <button className="accountBtn google" onClick={() => handleLogin("kakao")}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                  <path fillRule="evenodd" clipRule="evenodd" d="M19 10.2125C19 9.54776 18.9403 8.90856 18.8295 8.29492H10V11.9213H15.0455C14.8281 13.0932 14.1676 14.0861 13.1747 14.7509V17.1032H16.2045C17.9773 15.4711 19 13.0676 19 10.2125Z" fill="#4285F4"/>
-                  <path fillRule="evenodd" clipRule="evenodd" d="M9.99991 19.375C12.5312 19.375 14.6533 18.5355 16.2045 17.1037L13.1746 14.7514C12.3351 15.3139 11.2613 15.6463 9.99991 15.6463C7.55815 15.6463 5.49139 13.9972 4.75417 11.7812H1.62207V14.2102C3.16468 17.2741 6.33514 19.375 9.99991 19.375Z" fill="#34A853"/>
-                  <path fillRule="evenodd" clipRule="evenodd" d="M4.75426 11.781C4.56676 11.2185 4.46023 10.6177 4.46023 9.99978C4.46023 9.38188 4.56676 8.78103 4.75426 8.21853V5.78955H1.62216C0.987216 7.05518 0.625 8.48699 0.625 9.99978C0.625 11.5126 0.987216 12.9444 1.62216 14.21L4.75426 11.781Z" fill="#FBBC05"/>
-                  <path fillRule="evenodd" clipRule="evenodd" d="M9.99991 4.35369C11.3763 4.35369 12.6121 4.8267 13.5837 5.75568L16.2726 3.06676C14.6491 1.55398 12.5269 0.625 9.99991 0.625C6.33514 0.625 3.16468 2.72585 1.62207 5.78977L4.75417 8.21875C5.49139 6.00284 7.55815 4.35369 9.99991 4.35369Z" fill="#EA4335"/>
+                 <path d="M10 0C4.48 0 0 4.48 0 10c0 4.42 3.1 8.12 7.19 9.41.53.1.73-.23.73-.51v-1.74c-2.91.63-3.52-1.4-3.52-1.4-.48-1.24-1.18-1.57-1.18-1.57-.96-.66.07-.65.07-.65 1.06.07 1.62 1.09 1.62 1.09 1.02 1.74 2.67 1.24 3.32.95.1-.74.4-1.24.73-1.53-2.42-.28-4.97-1.21-4.97-5.39 0-1.19.42-2.16 1.11-2.93-.11-.28-.48-1.4.1-2.91 0 0 .91-.29 2.98 1.1a10.38 10.38 0 012.72-.36c.92 0 1.83.12 2.72.36 2.07-1.39 2.98-1.1 2.98-1.1.58 1.51.21 2.63.1 2.91.69.77 1.11 1.74 1.11 2.93 0 4.18-2.55 5.11-4.97 5.39.41.35.77 1.02.77 2.06v3.03c0 .28.2.61.73.51C16.9 18.12 20 14.42 20 10c0-5.52-4.48-10-10-10z" fill="#FEE500"/>
                 </svg>
                 <span>Sign up with Kakao</span>
               </button>
@@ -123,7 +120,7 @@ export default function Login() {
               validateStatus={fieldErrors.username ? fieldErrors.username.validateStatus : ''}
               help={fieldErrors.username ? fieldErrors.username.help : ''}
             >
-              <Input placeholder="Username" value={loginUserName} onChange={onUserNameHandler} />
+              <Input placeholder="Username" value={loginUserName} onChange={onUserNameHandler} autoComplete="new-password"/>
             </Form.Item>
             <Form.Item
               className="userAccount"
@@ -132,7 +129,7 @@ export default function Login() {
               validateStatus={fieldErrors.password ? fieldErrors.password.validateStatus : ''}
               help={fieldErrors.password ? fieldErrors.password.help : ''}
             >
-              <Input.Password placeholder="Password" value={loginPassword} onChange={onPasswordHandler} />
+              <Input.Password placeholder="Password" value={loginPassword} onChange={onPasswordHandler} autoComplete="new-password"/>
             </Form.Item>
             <Form.Item {...tailLayout}>
               <Button className="accountBtn basic" onClick={onClickLoginBtn} htmlType="submit" style={{ width: "100%" }}>
--- src/component/Header/signup/Signup.jsx
+++ src/component/Header/signup/Signup.jsx
@@ -1,41 +1,22 @@
-import { useContext, useState } from "react"
-import { AppContext } from "../../../context/AppContext"
+import { useContext, useState } from "react";
+import { AppContext } from "../../../context/AppContext";
 import { useNavigate } from "react-router-dom";
 import { Button, Card, Form, Input, notification } from "antd";
-import { FrownOutlined, SmileOutlined } from "@ant-design/icons";
-import { axiosInstance } from "../../../common/func/axios";
+import { axiosInstance } from '../../../common/func/axios';
+import "./Signup.css";
 
-let newFieldErrors;
-export default function Signup () {
+export default function Signup() {
+  const [form] = Form.useForm();
   const { signUpUserName, setSignUpUserName,
           signUpPassword, setSignUpPassword,
-          signUpEmail, setSignUpEmail,
-          signUpVerifyCode, setSignUpVerifyCode,
-          fieldErrors, setFieldErrors } = useContext(AppContext);
-  const [isDuplicated, setIsDuplicated] = useState(null);
+          signUpEmail, setSignUpEmail } = useContext(AppContext);
   const [passwordCheck, setPasswordCheck] = useState("");
-  const [validPwd, setValidPwd] = useState(false);
-
+  const [isUsernameAvailable, setIsUsernameAvailable] = useState(null);
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isCodeSent, setIsCodeSent] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isUsernameValid, setIsUsernameValid] = useState(false);
   const navigate = useNavigate();
-
-  const onUserNameHandler = (e) => {
-    setSignUpUserName(e.target.value);
-  };
-
-  const onPasswordHandler = (e) => {
-    setSignUpPassword(e.target.value);
-  };
-  const onPwdChkHandler = (e) => {
-    setPasswordCheck(e.target.value);
-  };
-
-  const onEmailHandler = (e) => {
-    setSignUpEmail(e.target.value);
-  };
-
-  const onEmailVerifyCodeHandler = (e) => {
-    setSignUpVerifyCode(e.target.value);
-  };
 
   const handleSignup = (type) => {
     const match = {
@@ -45,133 +26,87 @@ export default function Signup () {
     return (window.location.href = match[type]);
   };
 
-  const onClickSignupBtn = async (e) => {
-    const rules = signUpUserName !== "" && 
-                  signUpPassword !== "" && 
-                  passwordCheck !== "" && 
-                  signUpEmail !== "" &&
-                  validPwd
-    if(rules) {
-      
-    }
-    setFieldErrors({});
-    const data = { username: signUpUserName, password: signUpPassword, email: signUpEmail };
-
-    await axiosInstance.post('/api/users', data)
-    .then((res) => {
-      if(!isDuplicated && signUpUserName !== "" && signUpPassword !== "" && signUpEmail !== "" && signUpVerifyCode !== "") {
-        notification.open({
-          message: "회원가입 완료",
-          icon: <SmileOutlined style={{ color: "#108ee9" }} />
-        });
-        navigate("/");
-      }
-    })
-    .catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        const {
-          data: { errorMessage },
-        } = error.response;
-
-        notification.open({
-          message: "회원가입 실패",
-          description: errorMessage,
-          icon: <FrownOutlined style={{ color: "#ff3333" }} />
-        });
-
-        if (errorMessage) {
-          newFieldErrors = {
-            username: {
-              validateStatus: "error",
-              help: "Please input your username!",
-            },
-            password: {
-              validateStatus: "error",
-              help: "Please input your password!",
-            },
-            email: {
-              validateStatus: "error",
-              help: "Please input your email!",
-            }
-          };
-          setFieldErrors(newFieldErrors);
-        }
-      }
-    });
-  };
-
-  const onClickValidate = async () => {
-    if(signUpUserName !== "") {
-      const data = { username: signUpUserName };
-      await axiosInstance.post("/api/users/name/check", data) 
-        .then((res) => {
-          if(res.data.isDuplicated === true) {
-            setIsDuplicated(true);
-          } else if(res.data.isDuplicated === false) {
-            setIsDuplicated(false);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  };
-
-  const verification = async () => {
-    if(signUpEmail !== "") {
-      const data = { email: signUpEmail };
-      await axiosInstance.post("/api/users/email/code/send", data)
-        .then((res) => {
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-    }
-  };
-
-  const emailCheck = async () => {
-    if(signUpEmail !== "" && signUpVerifyCode !== "") {
-      const data = { email: signUpEmail, code: signUpVerifyCode };
-      await axiosInstance.post("/api/users/email/code/verify", data)
-        .then((res) => {
-          const data = res.data;
-          if(data.isVerified === true) {
-            notification.open({
-              message: "인증 완료",
-              icon: <SmileOutlined style={{ color: "#108ee9" }} />,
-            });
-          }
-        })
-        .catch((err)=> {
-          const {
-            data: { errorMessage },
-          } = err.response;
-          notification.open({
-            message: "인증 실패",
-            description: errorMessage,
-            icon: <FrownOutlined style={{ color: "#ff3333" }} />,
-          });
-        })
-    }
-  };
-
   const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 10}
+    labelCol: { span: 24 },
+    wrapperCol: { span: 24 }
   };
-    
+
   const tailLayout = {
-    wrapperCol: { offset: 11}
+    wrapperCol: { offset: 0, span: 24 }
+  };
+
+  // 회원 가입
+  const onFinish = (values) => {
+    try {
+      const data = { username: signUpUserName, email: signUpEmail, password: signUpPassword };
+      axiosInstance.post("/api/users", data);
+      notification.success({ message: '회원 가입 더럽게 힘드네!' });
+      navigate("/login");
+    } catch (error) {
+      notification.error({ message: '회원 가입 실패' });
+    }
+  };
+
+  // 이름 중복 검증
+  const checkUsername = async () => {
+    try {
+      const data = { username: signUpUserName };
+      const response = await axiosInstance.post("/api/users/name/check", data);  
+
+      if (!response.data.isDuplicate) {
+        setIsUsernameAvailable(true);
+        setIsUsernameValid(true);
+        form.setFieldsValue({ username: signUpUserName });
+        notification.success({ message: 'Username is available!' });
+      } else {
+        setIsUsernameAvailable(false);
+        notification.error({ message: 'Username is already taken!' });
+      }
+    } catch (error) {
+      notification.error({ message: 'Error checking username!' });
+    }
+  };
+
+  // 이메일 인증 코드 전송
+  const sendVerificationCode = async () => {
+    try {
+      const data = { email: signUpEmail };
+      await axiosInstance.post("/api/users/email/code/send", data);  
+      notification.success({ message: 'Verification code sent to your email!' });
+      setIsCodeSent(true); 
+    } catch (error) {
+      notification.error({ message: 'Error sending verification code!' });
+    }
+  };
+
+  // 이메일 인증 검증 
+  const verifyCode = async () => {
+    try {
+      const data = { email: signUpEmail, code: verificationCode };
+      const response = await axiosInstance.post("/api/users/email/code/verify", data);
+      console.log(response.data);
+
+      if (response.data.isVerified) {
+        notification.success({ message: 'Email Verification Success!' });
+        setIsCodeSent(false);
+        form.setFieldsValue({ email: signUpEmail });
+        form.setFieldsValue({ verificationCode: verificationCode });
+        setIsEmailValid(true);
+      } else {
+        notification.error({ message: 'Invaild verification code!' });
+      }
+    } catch (error) {
+      notification.error({ message: 'Invaild verification code!' });
+    }
   };
 
   return (
     <div className="Account">
       <div className="header"></div>
       <div className="content">
-        <img src="/img/DNALogin.png" />
+        <img src="/img/DNALogin.png" alt="Logo" />
         <Card className="accountCnt signup" title={<span className="tit">Sign Up</span>}>
+
           <Form {...layout} autoComplete={"false"}>
             <Form.Item {...tailLayout}>
               <button className="accountBtn google" onClick={() => handleSignup("google")}>
@@ -187,74 +122,153 @@ export default function Signup () {
             <Form.Item {...tailLayout}>
               <button className="accountBtn google" onClick={() => handleSignup("kakao")}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                  <path fillRule="evenodd" clipRule="evenodd" d="M19 10.2125C19 9.54776 18.9403 8.90856 18.8295 8.29492H10V11.9213H15.0455C14.8281 13.0932 14.1676 14.0861 13.1747 14.7509V17.1032H16.2045C17.9773 15.4711 19 13.0676 19 10.2125Z" fill="#4285F4"/>
-                  <path fillRule="evenodd" clipRule="evenodd" d="M9.99991 19.375C12.5312 19.375 14.6533 18.5355 16.2045 17.1037L13.1746 14.7514C12.3351 15.3139 11.2613 15.6463 9.99991 15.6463C7.55815 15.6463 5.49139 13.9972 4.75417 11.7812H1.62207V14.2102C3.16468 17.2741 6.33514 19.375 9.99991 19.375Z" fill="#34A853"/>
-                  <path fillRule="evenodd" clipRule="evenodd" d="M4.75426 11.781C4.56676 11.2185 4.46023 10.6177 4.46023 9.99978C4.46023 9.38188 4.56676 8.78103 4.75426 8.21853V5.78955H1.62216C0.987216 7.05518 0.625 8.48699 0.625 9.99978C0.625 11.5126 0.987216 12.9444 1.62216 14.21L4.75426 11.781Z" fill="#FBBC05"/>
-                  <path fillRule="evenodd" clipRule="evenodd" d="M9.99991 4.35369C11.3763 4.35369 12.6121 4.8267 13.5837 5.75568L16.2726 3.06676C14.6491 1.55398 12.5269 0.625 9.99991 0.625C6.33514 0.625 3.16468 2.72585 1.62207 5.78977L4.75417 8.21875C5.49139 6.00284 7.55815 4.35369 9.99991 4.35369Z" fill="#EA4335"/>
+                  <path d="M10 0C4.48 0 0 4.48 0 10c0 4.42 3.1 8.12 7.19 9.41.53.1.73-.23.73-.51v-1.74c-2.91.63-3.52-1.4-3.52-1.4-.48-1.24-1.18-1.57-1.18-1.57-.96-.66.07-.65.07-.65 1.06.07 1.62 1.09 1.62 1.09 1.02 1.74 2.67 1.24 3.32.95.1-.74.4-1.24.73-1.53-2.42-.28-4.97-1.21-4.97-5.39 0-1.19.42-2.16 1.11-2.93-.11-.28-.48-1.4.1-2.91 0 0 .91-.29 2.98 1.1a10.38 10.38 0 012.72-.36c.92 0 1.83.12 2.72.36 2.07-1.39 2.98-1.1 2.98-1.1.58 1.51.21 2.63.1 2.91.69.77 1.11 1.74 1.11 2.93 0 4.18-2.55 5.11-4.97 5.39.41.35.77 1.02.77 2.06v3.03c0 .28.2.61.73.51C16.9 18.12 20 14.42 20 10c0-5.52-4.48-10-10-10z" fill="#FEE500"/>
                 </svg>
                 <span>Sign up with Kakao</span>
               </button>
             </Form.Item>
             <span className="description" style={{margin: "0rem"}}>or sign up with Email</span>
+          </Form>
+
+
+          {/* 회원 가입 폼 */}
+          <Form form={form} autoComplete="off" onFinish={onFinish} initialValues={{ username: '' }}>
+            
+            {/* 이름 중복 검증 */}
             <Form.Item
-              className="userAccount"
-              label={<span className="userTit">Username</span>}
+              label="Username"
               name="username"
-              hasFeedback
-              validateStatus={fieldErrors.username ? fieldErrors.username.validateStatus : ''}
-              help={fieldErrors.username ? fieldErrors.username.help : ''}
+              {...layout}
+              rules={[
+                { required: true, message: 'Username is required!' },
+                () => ({
+                  validator(_, value) {
+                    if (isUsernameAvailable === false) {
+                      return Promise.reject(new Error('Username is already taken!'));
+                    }
+                    return Promise.resolve();
+                  },
+                }),
+              ]}
             >
-              <Input placeholder="Username" value={signUpUserName} onChange={onUserNameHandler}/>
-              <button className="chkBtn" onClick={onClickValidate}>validate check</button>
+              <Input 
+                placeholder="Enter your username" 
+                onChange={(e) => setSignUpUserName(e.target.value)} 
+                style={{ width: '100%' }}
+                autoComplete="new-password"
+              />
+              {!isUsernameValid && (
+                <Button 
+                  onClick={checkUsername} 
+                  style={{ marginTop: '8px', width: '100%' }}
+                >
+                  Check Availability
+                </Button>
+              )}
             </Form.Item>
+            
+            {/* 이메일 인증 */}
             <Form.Item
-              className="userAccount"
-              label={<span className="userTit">Email</span>}
+              label="Email"
               name="email"
-              hasFeedback
-              validateStatus={fieldErrors.email ? fieldErrors.email.validateStatus : ''}
-              help={fieldErrors.email ? fieldErrors.email.help : ''}
+              {...layout}
+              rules={[
+                { required: true, message: 'Email is required!' }, 
+                { type: 'email', message: 'Please enter a valid email!' }
+              ]}
             >
-              <Input placeholder="@Email.com" value={signUpEmail} onChange={onEmailHandler}/>
-              <button className="chkBtn" onClick={verification}>Verification</button>
+              <Input 
+                placeholder="Enter your email" 
+                onChange={(e) => setSignUpEmail(e.target.value)} 
+                style={{ width: '100%' }}
+                autoComplete="new-password"
+              />
+              {!isEmailValid && ( 
+                <Button 
+                  onClick={sendVerificationCode} 
+                  style={{ marginTop: '8px', width: '100%' }}
+                >
+                Send Verification Code
+                </Button>
+              )}
             </Form.Item>
+            <div className={`fade-in ${isCodeSent ? 'fade-in-active' : ''}`}>
+            {isCodeSent && ( 
+              <>
+                <Form.Item
+                  label="Verification Code"
+                  name="verificationCode"
+                  {...layout}
+                  rules={[{ required: true, message: 'Verification code is required!' }]}
+                >
+                  <Input 
+                    placeholder="Enter your verification code" 
+                    onChange={(e) => setVerificationCode(e.target.value)} 
+                    style={{ width: '100%' }}
+                  />
+                </Form.Item>
+                <Button 
+                  onClick={verifyCode} 
+                  style={{ marginTop: '8px', width: '100%' }}
+                >
+                  Verify Code
+                </Button>
+              </>
+            )}
+            </div>
+            
+            {/* 비밀번호 확인 */}
             <Form.Item
-              className="userAccount"
-              name="verify"
-              style={{height: "53px"}}
-              hasFeedback
-            >
-              <Input placeholder="Verification Code" value={signUpVerifyCode} onChange={onEmailVerifyCodeHandler} />
-              <button className="chkBtn" onClick={emailCheck}>Check</button>
-            </Form.Item>
-            <Form.Item
-              className="userAccount"
-              label={<span className="userTit">Password</span>}
+              label="Password"
               name="password"
-              validateStatus={fieldErrors.password ? fieldErrors.password.validateStatus : ''}
-              help={fieldErrors.password ? fieldErrors.password.help : ''}
+              {...layout}
+              rules={[{ required: true, message: 'Password is required!' },
+              { min: 6, message: 'Password must be at least 6 characters long!'}]}
             >
-              <Input.Password placeholder="Password" value={signUpPassword} onChange={onPasswordHandler} />
-              <Input.Password placeholder="Password Check" value={passwordCheck} onChange={onPwdChkHandler} />
+              <Input.Password 
+                placeholder="Enter your password" 
+                onChange={(e) => setSignUpPassword(e.target.value)} 
+                style={{ width: '100%' }}
+                autoComplete="new-password"
+              />
             </Form.Item>
-            <Form.Item {...tailLayout} style={{marginBottom: "0px"}}>
-              <Button className="accountBtn basic" onClick={onClickSignupBtn} htmlType="submit" style={{ width: '100%' }}>
-                Create Account
+            <div className={`fade-in ${signUpPassword ? 'fade-in-active' : ''}`}>
+            {signUpPassword && (
+            <Form.Item
+              label="Confirm Password"
+              name="confirmPassword"
+              {...layout}
+              rules={[
+                { required: true, message: 'Please confirm your password!' },
+                () => ({
+                  validator(_, value) {
+                    if (!value || value === signUpPassword) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('The two passwords do not match!'));
+                  },
+                }),
+              ]}
+            >
+              <Input.Password 
+                placeholder="Confirm your password" 
+                onChange={(e) => setPasswordCheck(e.target.value)} 
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+            )}
+            </div>
+
+            <Form.Item {...tailLayout}>
+              <Button className="accountBtn basic" htmlType="submit" style={{ width: "100%", marginTop: '20px' }}>
+                Sign Up
               </Button>
             </Form.Item>
-            <Form.Item className="etcCtn" {...tailLayout}>
-              <span className="etc">Already have an account?</span>
-              <div className="etcBtn" onClick={() => navigate("/login")}>
-                <span>Log in</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                  <path d="M2.5 6.85498L11.1741 6.85498" stroke="black" strokeWidth="0.75"/>
-                  <path d="M7.64502 3L11.5002 6.85515L7.64502 10.7103" stroke="black" strokeWidth="0.75"/>
-                </svg>
-              </div>
-            </Form.Item>
+
           </Form>
+
         </Card>
       </div>
     </div>
-  )
-};
+  );
+}
