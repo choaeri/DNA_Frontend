b8f63d5abc9c4c914309e42eb37f5cb46ce264ac
choaeri <choyd00@naver.com>
2024-08-31 18:50:54 +0900

    mypage schedule 수정

--- src/component/Header/Schedule/Schedule.jsx
+++ src/component/Header/Schedule/Schedule.jsx
@@ -5,6 +5,17 @@ export default function Schedule () {
   return (
     <div className="Schedule">
       <Header />
+      <div className="schdCnt">
+        <span>Schedule</span>
+        <span>schedules, Date order</span>
+        <div className="item">
+          <img />
+          <div className="info">
+                    
+          </div>
+          <button>Write a review</button>
+        </div>
+      </div>
     </div>
   );
 };
