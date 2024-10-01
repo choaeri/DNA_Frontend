ff7a33985df3ffa7fbbd503fe746a225ad588d0d
choaeri <choyd00@naver.com>
2024-10-01 18:16:37 +0900

    에러 및 화면 수정

--- src/component/Header/Likes/Likes.jsx
+++ src/component/Header/Likes/Likes.jsx
@@ -1,232 +1,261 @@
-import { useContext, useEffect, useState } from "react";
-import Header from "../Header";
-import useLocalStorage from "../../../utils/useLocalStorage";
-import { AppContext } from "../../../context/AppContext";
-import { axiosInstance } from "../../../common/func/axios";
-import { categoryMatch } from "../../../common/func/match";
-import "./Likes.css";
-import { HeartFilled, HeartOutlined } from "@ant-design/icons";
-import WorkationDetail from "../../DetailLocation/LocationMap/WorkationDetail/WorkationDetail";
+import React, { useContext, useEffect, useState } from 'react';
+import Header from '../Header';
+import useLocalStorage from '../../../utils/useLocalStorage';
+import { AppContext } from '../../../context/AppContext';
+import { axiosInstance } from '../../../common/func/axios';
+import { categoryMatch, locationMatch } from '../../../common/func/match';
+import './Likes.css';
+import { HeartFilled, HeartOutlined } from '@ant-design/icons';
+import WorkationDetail from '../../DetailLocation/LocationMap/WorkationDetail/WorkationDetail';
 
 const locationImg = [
-	"/img/workation1.webp",
-	"/img/workation2.webp",
-	"/img/workation3.webp",
-	"/img/workation4.webp",
-	"/img/workation5.webp",
+	'/img/workation1.webp',
+	'/img/workation2.webp',
+	'/img/workation3.webp',
+	'/img/workation4.webp',
+	'/img/workation5.webp',
 ];
 export default function Likes() {
-  const {
-    isBookmarked,
-    setIsBookmarked,
-    isWorkationBookmarked,
-    setIsWorkationBookmarked,
-    setWorkationModal,
-    onClickLike,
-    errMessageCheck,
-  } = useContext(AppContext);
-  const [bookmarkList, setBookmarkList] = useState();
-  const [bookmarkOfficeList, setBookmarkOfficeList] = useState();
-  const [selectedBookmark, setSelectedBookmark] = useState(null);
+	const {
+		isBookmarked,
+		setIsBookmarked,
+		isWorkationBookmarked,
+		setIsWorkationBookmarked,
+		setWorkationModal,
+		onClickLike,
+		errMessageCheck,
+	} = useContext(AppContext);
+	const [bookmarkList, setBookmarkList] = useState();
+	const [bookmarkOfficeList, setBookmarkOfficeList] = useState();
+	const [selectedBookmark, setSelectedBookmark] = useState(null);
 
-  const newIsBookmarked = {};
-  // 북마크 시설
-  const fetchBookmark = async (facilityId) => {
-    const apiUrl = `/api/facilities/${facilityId}/bookmark`;
-    try {
-      const response = await axiosInstance.get(apiUrl);
-      const data = response.data;
+	const newIsBookmarked = {};
+	// 북마크 시설
+	const fetchBookmark = async (facilityId) => {
+		const apiUrl = `/api/facilities/${facilityId}/bookmark`;
+		try {
+			const response = await axiosInstance.get(apiUrl);
+			const data = response.data;
 
-      newIsBookmarked[facilityId] = data.isBookmarked;
-    } catch (error) {
-      errMessageCheck(error.response.data.errorMessage);
-    }
-  };
+			newIsBookmarked[facilityId] = data.isBookmarked;
+		} catch (error) {
+			errMessageCheck(error.response.data.errorMessage);
+		}
+	};
 
-  // 북마크 워케이션 시설
-  const newIsBookmarkedOffice = {};
-  const fetchBookmarkOffice = async (facilityId) => {
-    const apiUrl = `/api/workation-offices/${facilityId}/bookmark`;
-    try {
-      const response = await axiosInstance.get(apiUrl);
-      const data = response.data;
-      newIsBookmarkedOffice[facilityId] = data.isBookmarked;
-    } catch (error) {
-      errMessageCheck(error.response.data.errorMessage);
-    }
-  };
+	// 북마크 워케이션 시설
+	const newIsBookmarkedOffice = {};
+	const fetchBookmarkOffice = async (facilityId) => {
+		const apiUrl = `/api/workation-offices/${facilityId}/bookmark`;
+		try {
+			const response = await axiosInstance.get(apiUrl);
+			const data = response.data;
+			newIsBookmarkedOffice[facilityId] = data.isBookmarked;
+		} catch (error) {
+			errMessageCheck(error.response.data.errorMessage);
+		}
+	};
 
-  useEffect(() => {
-    const fetchBookmarkList = async () => {
-      const apiUrl = `/api/facilities/bookmark`;
-      try {
-        const res = await axiosInstance.get(apiUrl);
-        const data = res.data;
-        if (Array.isArray(data)) {
-          const groupedBookmarks = data.reduce((acc, bookmark) => {
-            const location = bookmark.locationName;
-            if (!acc[location]) {
-              acc[location] = [];
-            }
-            acc[location].push(bookmark);
-            return acc;
-          }, {});
-          setBookmarkList(groupedBookmarks);
+	useEffect(() => {
+		const fetchBookmarkList = async () => {
+			const apiUrl = `/api/facilities/bookmark`;
+			try {
+				const res = await axiosInstance.get(apiUrl);
+				const data = res.data;
+				if (Array.isArray(data)) {
+					const groupedBookmarks = data.reduce((acc, bookmark) => {
+						const location = bookmark.locationName;
+						if (!acc[location]) {
+							acc[location] = [];
+						}
+						acc[location].push(bookmark);
+						return acc;
+					}, {});
+					setBookmarkList(groupedBookmarks);
 
-          await Promise.all(data.map((item) => fetchBookmark(item.facilityId)));
-          setIsBookmarked(newIsBookmarked);
-        }
-      } catch (error) {
-        errMessageCheck(error.response.data.errorMessage);
-      }
-    };
+					await Promise.all(data.map((item) => fetchBookmark(item.facilityId)));
+					setIsBookmarked(newIsBookmarked);
+				}
+			} catch (error) {
+				errMessageCheck(error.response.data.errorMessage);
+			}
+		};
 
-    const fetchBookmarkOfficeList = async () => {
-      const apiUrl = `/api/workation-offices/bookmark`;
-      try {
-        const res = await axiosInstance.get(apiUrl);
-        const data = res.data;
-        if (Array.isArray(data)) {
-          const groupedBookmarks = data.reduce((acc, bookmark) => {
-            const location = bookmark.locationName;
-            if (!acc[location]) {
-              acc[location] = [];
-            }
-            acc[location].push(bookmark);
-            return acc;
-          }, {});
-          setBookmarkOfficeList(groupedBookmarks);
-          await Promise.all(
-            data.map((item) => fetchBookmarkOffice(item.officeId))
-          );
-          setIsWorkationBookmarked(newIsBookmarkedOffice);
-        }
-      } catch (error) {
-        errMessageCheck(error.response.data.errorMessage);
-      }
-    };
+		const fetchBookmarkOfficeList = async () => {
+			const apiUrl = `/api/workation-offices/bookmark`;
+			try {
+				const res = await axiosInstance.get(apiUrl);
+				const data = res.data;
+				if (Array.isArray(data)) {
+					const groupedBookmarks = data.reduce((acc, bookmark) => {
+						const location = bookmark.locationName;
+						if (!acc[location]) {
+							acc[location] = [];
+						}
+						acc[location].push(bookmark);
+						return acc;
+					}, {});
+					setBookmarkOfficeList(groupedBookmarks);
 
-    fetchBookmarkList();
-    fetchBookmarkOfficeList();
-  }, []);
+					await Promise.all(
+						data.map((item) => fetchBookmarkOffice(item.officeId))
+					);
 
-  const handleOpenDetail = (bookmark) => {
-    setSelectedBookmark(bookmark); 
-    setWorkationModal(true);
-  };
+					console.log('newIsBookmarkedOffice:', newIsBookmarkedOffice); // Log here
+					setIsWorkationBookmarked(newIsBookmarkedOffice);
+				}
+			} catch (error) {
+				errMessageCheck(error.response.data.errorMessage);
+			}
+		};
 
-  return (
-    <div className="Likes">
-      <Header />
-      <div className="bmkCnt">
-        <div className="header">
-          <span className="tit">Likes</span>
-        </div>
-        {bookmarkOfficeList && Object.keys(bookmarkOfficeList).length > 0
-          ? Object.keys(bookmarkOfficeList).map((locationName, index) => (
-              <div className="content" key={index}>
-                <span className="lcnName">{locationName}</span>
-                <div className="card wkn">
-                  {bookmarkOfficeList[locationName].map((bookmark, idx) => {
-                    return bookmark.type === "Workation Office" ? (
-                      <div className="item work" key={idx}>
-                        {isWorkationBookmarked &&
-                        isWorkationBookmarked[bookmark.officeId] ? (
-                          <HeartFilled
-                            className="icon"
-                            onClick={(e) => onClickLike(e, bookmark.officeId, 'Workation Office')}
-                            style={{ color: "red" }}
-                          />
-                        ) : (
-                          <HeartOutlined
-                            className="icon"
-                            onClick={(e) => onClickLike(e, bookmark.officeId, 'Workation Office')}
-                            style={{ color: "black" }}
-                          />
-                        )}
-                        <div className="imgCnt">
-                          <img src={locationImg[bookmark.officeId % 5]}>
-                          </img>
-                        </div>
-                        <div className="like wkn">
-                          <div className="exp">
-                            <span className="type">
-                              {categoryMatch[bookmark.type].type}
-                            </span>
-                            <span className="name">{bookmark.officeName}</span>
-                          </div>
-                          <svg
-                            onClick={() => handleOpenDetail(bookmark)}
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="12"
-                            height="13"
-                            viewBox="0 0 12 13"
-                            fill="none"
-                          >
-                            <path
-                              d="M8.64795 6.89795L4.89795 10.6479C4.79228 10.7536 4.64895 10.813 4.49951 10.813C4.35007 10.813 4.20675 10.7536 4.10107 10.6479C3.9954 10.5423 3.93604 10.399 3.93604 10.2495C3.93604 10.1001 3.9954 9.95675 4.10107 9.85107L7.4531 6.49998L4.10201 3.14795C4.04969 3.09562 4.00818 3.03351 3.97986 2.96514C3.95155 2.89678 3.93697 2.82351 3.93697 2.74951C3.93697 2.67551 3.95155 2.60224 3.97986 2.53388C4.00818 2.46551 4.04969 2.4034 4.10201 2.35107C4.15433 2.29875 4.21645 2.25724 4.28482 2.22893C4.35318 2.20061 4.42645 2.18604 4.50045 2.18604C4.57445 2.18604 4.64772 2.20061 4.71608 2.22893C4.78445 2.25724 4.84656 2.29875 4.89889 2.35107L8.64889 6.10107C8.70126 6.15339 8.7428 6.21554 8.77111 6.28394C8.79942 6.35235 8.81395 6.42567 8.81386 6.49971C8.81377 6.57374 8.79907 6.64703 8.7706 6.71537C8.74213 6.7837 8.70045 6.84575 8.64795 6.89795Z"
-                              fill="#292C30"
-                            />
-                          </svg>
-                        </div>
-                        {selectedBookmark && selectedBookmark.officeId === bookmark.officeId && (
-                          <WorkationDetail
-                            locationName={bookmark.locationName}
-                            officeName={bookmark.officeName}
-                            officeId={bookmark.officeId}
-                          />
-                        )}
-                      </div>
-                    ) : null;
-                  })}
-                </div>
-              </div>
-            ))
-          : null}
-        {bookmarkList && Object.keys(bookmarkList).length > 0
-          ? Object.keys(bookmarkList).map((locationName, index) => (
-              <div className="content" key={index}>
-                <div className="card etc">
-                  {bookmarkList[locationName].map((bookmark, idx) => {
-                    return bookmark.type !== "workation" ? (
-                      <div className="item etc" key={idx}>
-                        <span className="type">
-                          {categoryMatch[bookmark.type].type}
-                        </span>
-                        <div className="like">
-                          <span className="name">{bookmark.facilityName}</span>
-                          {isBookmarked && isBookmarked[bookmark.facilityId] ? (
-                            <HeartFilled
-                              onClick={(e) =>
-                                onClickLike(e, bookmark.facilityId)
-                              }
-                              style={{
-                                color: "red",
-                                cursor: "pointer",
-                                marginLeft: "auto",
-                              }}
-                            />
-                          ) : (
-                            <HeartOutlined
-                              onClick={(e) =>
-                                onClickLike(e, bookmark.facilityId)
-                              }
-                              style={{
-                                color: "black",
-                                cursor: "pointer",
-                                marginLeft: "auto",
-                              }}
-                            />
-                          )}
-                        </div>
-                      </div>
-                    ) : null;
-                  })}
-                </div>
-              </div>
-            ))
-          : null}
-      </div>
-    </div>
-  );
+		fetchBookmarkList();
+		fetchBookmarkOfficeList();
+	}, []);
+
+	const handleOpenDetail = (bookmark) => {
+		setSelectedBookmark(bookmark);
+		setWorkationModal(true);
+	};
+
+	return (
+		<div className="Likes">
+			<Header />
+			<div className="bmkCnt">
+				<div className="header">
+					<span className="tit">Likes</span>
+				</div>
+				{bookmarkOfficeList && bookmarkList && (
+					<div className="content">
+						{Object.keys({ ...bookmarkList, ...bookmarkOfficeList }).map(
+							(locationName, index) => (
+								<React.Fragment key={index}>
+									<span className="lcnName">{locationMatch[locationName]}</span>
+									<div className="card wkn" key={index}>
+										{bookmarkOfficeList[locationName] &&
+											bookmarkOfficeList[locationName].map(
+												(bookmark, idx) =>
+													bookmark.type === 'Workation Office' && (
+														<div className="item work" key={idx}>
+															{isWorkationBookmarked &&
+															isWorkationBookmarked[bookmark.officeId] ? (
+																<HeartFilled
+																	className="icon"
+																	onClick={(e) =>
+																		onClickLike(
+																			e,
+																			bookmark.officeId,
+																			'Workation Office'
+																		)
+																	}
+																	style={{ color: 'red' }}
+																/>
+															) : (
+																<HeartOutlined
+																	className="icon"
+																	onClick={(e) =>
+																		onClickLike(
+																			e,
+																			bookmark.officeId,
+																			'Workation Office'
+																		)
+																	}
+																	style={{ color: 'black' }}
+																/>
+															)}
+															<div className="imgCnt">
+																<img
+																	src={locationImg[bookmark.officeId % 5]}
+																	alt="location"
+																/>
+															</div>
+															<div
+																className="like wkn"
+																onClick={() => handleOpenDetail(bookmark)}
+															>
+																<div className="exp">
+																	<span className="type">
+																		{categoryMatch[bookmark.type].type}
+																	</span>
+																	<span className="name">
+																		{bookmark.officeName}
+																	</span>
+																</div>
+																<svg
+																	xmlns="http://www.w3.org/2000/svg"
+																	width="12"
+																	height="13"
+																	viewBox="0 0 12 13"
+																	fill="none"
+																>
+																	<path
+																		d="M8.64795 6.89795L4.89795 10.6479C4.79228 10.7536 4.64895 10.813 4.49951 10.813C4.35007 10.813 4.20675 10.7536 4.10107 10.6479C3.9954 10.5423 3.93604 10.399 3.93604 10.2495C3.93604 10.1001 3.9954 9.95675 4.10107 9.85107L7.4531 6.49998L4.10201 3.14795C4.04969 3.09562 4.00818 3.03351 3.97986 2.96514C3.95155 2.89678 3.93697 2.82351 3.93697 2.74951C3.93697 2.67551 3.95155 2.60224 3.97986 2.53388C4.00818 2.46551 4.04969 2.4034 4.10201 2.35107C4.15433 2.29875 4.21645 2.25724 4.28482 2.22893C4.35318 2.20061 4.42645 2.18604 4.50045 2.18604C4.57445 2.18604 4.64772 2.20061 4.71608 2.22893C4.78445 2.25724 4.84656 2.29875 4.89889 2.35107L8.64889 6.10107C8.70126 6.15339 8.7428 6.21554 8.77111 6.28394C8.79942 6.35235 8.81395 6.42567 8.81386 6.49971C8.81377 6.57374 8.79907 6.64703 8.7706 6.71537C8.74213 6.7837 8.70045 6.84575 8.64795 6.89795Z"
+																		fill="#292C30"
+																	/>
+																</svg>
+															</div>
+															{selectedBookmark &&
+																selectedBookmark.officeId ===
+																	bookmark.officeId && (
+																	<WorkationDetail
+																		locationName={bookmark.locationName}
+																		officeName={bookmark.officeName}
+																		officeId={bookmark.officeId}
+																	/>
+																)}
+														</div>
+													)
+											)}
+									</div>
+									<div className="card etc" key={index}>
+										{/* Render Other Facility Bookmarks */}
+										{bookmarkList[locationName] &&
+											bookmarkList[locationName].map(
+												(bookmark, idx) =>
+													bookmark.type !== 'workation' && (
+														<div className="item etc" key={idx}>
+															<span className="type">
+																{categoryMatch[bookmark.type].type}
+															</span>
+															<div className="like">
+																<span className="name">
+																	{bookmark.facilityName}
+																</span>
+																{isBookmarked &&
+																isBookmarked[bookmark.facilityId] ? (
+																	<HeartFilled
+																		onClick={(e) =>
+																			onClickLike(e, bookmark.facilityId)
+																		}
+																		style={{
+																			color: 'red',
+																			cursor: 'pointer',
+																			marginLeft: 'auto',
+																		}}
+																	/>
+																) : (
+																	<HeartOutlined
+																		onClick={(e) =>
+																			onClickLike(e, bookmark.facilityId)
+																		}
+																		style={{
+																			color: 'black',
+																			cursor: 'pointer',
+																			marginLeft: 'auto',
+																		}}
+																	/>
+																)}
+															</div>
+														</div>
+													)
+											)}
+									</div>
+								</React.Fragment>
+							)
+						)}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
--- src/component/Header/RecommendAreas/RecommendAreas.jsx
+++ src/component/Header/RecommendAreas/RecommendAreas.jsx
@@ -9,9 +9,10 @@ import { useNavigate } from "react-router-dom";
 import { AppContext } from '../../../context/AppContext';
 
 const { kakao } = window;
-export default function RecommendAreas () {
+
+export default function RecommendAreas() {
   const [step, setStep] = useState(0);
-  const [recommendedArea, setRecommendedArea] = useState(null);
+  const [recommendedArea, setRecommendedArea] = useState([]);  // Initialize with an empty array
   const [centerMarker, setCenterMarker] = useState({
     lat: 0,
     lng: 0
@@ -20,73 +21,82 @@ export default function RecommendAreas () {
   const { errMessageCheck } = useContext(AppContext);
 
   const geocoder = new window.kakao.maps.services.Geocoder();
+
   const getLocationByAddress = async (address) => {
     geocoder.addressSearch(address, (result, status) => {
-      if(status === kakao.maps.services.Status.OK) {
+      if (status === kakao.maps.services.Status.OK) {
         const coord = new kakao.maps.LatLng(result[0].y, result[0].x);
         setCenterMarker({
           lat: coord.Ma,
           lng: coord.La
-        })
-      };
+        });
+      }
     });
   };
-  
+
   useEffect(() => {
     getLocationByAddress('강원');
     const getRecommendedAreas = async () => {
       try {
         const res = await axiosInstance.get('/api/recommend/locations');
-        setRecommendedArea(res.data);
+        const data = res.data;
+        setRecommendedArea(data);
       } catch (err) {
         errMessageCheck(err.response.data.errorMessage);
-      };
+      }
     };
     getRecommendedAreas();
-  }, []);
+  }, [errMessageCheck]);
 
   return (
-    <>
-        <div className="RecommendAreas">
-          <Header />
-          { recommendedArea ? (
-            <div className="rmdAreaCnt">
-              <div className="header">
-                <span className="tit">Recommended Areas</span>
-              </div>
-              <div className="content">
-                <InfoAreas 
+    <div className="RecommendAreas">
+      <Header />
+      {recommendedArea.length > 0 ? (
+        <div className="rmdAreaCnt">
+          <div className="header">
+            <span className="tit">Recommended Areas</span>
+          </div>
+          <div className="content">
+            <InfoAreas
+              step={step}
+              setStep={setStep}
+              recommendedArea={recommendedArea}
+            />
+            <div className="mapArea">
+              <Map
+                className="map"
+                center={centerMarker}
+                level={12}
+              >
+                <MapAreas
                   step={step}
-                  setStep={setStep}
-                  recommendedArea={recommendedArea} 
+                  recommendedArea={recommendedArea}
                 />
-                <div className="mapArea">
-                  <Map
-                    className="map"
-                    center={centerMarker}
-                    level={12}
-                  >
-                    <MapAreas 
-                      step={step}
-                      recommendedArea={recommendedArea} 
-                    />
-                  </Map>
-                </div>
-              </div>
-              <div className="footer" onClick={() => navigate("/survey")}>
-                <span>Test again</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
-                  <path d="M3 6.85547L11.6741 6.85547" stroke="black" strokeWidth="0.75"/>
-                  <path d="M8.14502 3L12.0002 6.85515L8.14502 10.7103" stroke="black" strokeWidth="0.75"/>
-                </svg>
-              </div>
+              </Map>
             </div>
-          ) : 
-          <div className="rmdAreaCnt">
-            <span className="tit">Discover Amazing Destinations!</span>
-            <span className="desp">Looking for your next travel adventure?<br />Explore these incredible places that offer a mix of culture, history, and unforgettable experiences.<br />Whether you're seeking a trendy urban vibe or a serene nature escape, we've got the perfect spots for you.<br />Get inspired and start planning your dream trip today!</span>
-          </div> }
+          </div>
+          <div className="footer" onClick={() => navigate("/survey")}>
+            <span>Test again</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
+              <path d="M3 6.85547L11.6741 6.85547" stroke="black" strokeWidth="0.75" />
+              <path d="M8.14502 3L12.0002 6.85515L8.14502 10.7103" stroke="black" strokeWidth="0.75" />
+            </svg>
+          </div>
         </div>
-    </>
-  )
-};
+      ) : recommendedArea.length === 0 ? (
+        <div className="rmdAreaCnt">
+          <span className="tit">Discover Amazing Destinations!</span>
+          <span className="desp">
+            Looking for your next travel adventure?
+            <br />
+            Explore these incredible places that offer a mix of culture, history, and unforgettable experiences.
+            <br />
+            Whether you're seeking a trendy urban vibe or a serene nature escape, we've got the perfect spots for you.
+            <br />
+            Get inspired and start planning your dream trip today!
+          </span>
+        </div>
+      ) : null}
+    </div>
+  );
+}
